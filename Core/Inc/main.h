40d85cf9422a3d88d5e2c64d58a465e5831a2879
crystal-net <gckemper@gmail.com>
2024-05-19 15:55:15 -0700

    updated gitignore to remove build folders

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -310,13 +310,24 @@ static void MX_DMA_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
@@ -339,7 +350,9 @@ void StartDefaultTask(void *argument)
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+  	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+
+  	osDelay(25);
   }
   /* USER CODE END 5 */
 }
@@ -357,8 +370,9 @@ void StartBlinkLD2(void *argument)
   /* Infinite loop */
   for(;;)
   {
+  	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 
-  	osDelay(1);
+  	osDelay(250);
   }
   /* USER CODE END StartBlinkLD2 */
 }
